04f8339ba4bc0366b1674509a91f72c48a30baf2
isabella.poles <isabella.poles@gmail.com>
2025-03-30 14:07:22 +0000

    SR-microCT data pre-processing utils update

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 """Utility objects for processing - training - logging."""
 import os
+import cv2
 import wandb
+import bisect
 import importlib
 
 import torch 
@@ -8,7 +10,11 @@ import torch
 import numpy as np
 import nibabel as nib
 
-from skimage.morphology import disk, binary_dilation, binary_closing
+from PIL import Image
+from scipy import ndimage
+from skimage import filters
+from scipy.ndimage import gaussian_filter
+from skimage.morphology import disk, square, binary_dilation, binary_closing
 
 from models.unet import UNet
 
@@ -146,3 +152,104 @@ def load_separate_model(configs, device, checkpoint_s, checkpoint_t):
         model_s = model_s.to(device)
 
     return model_t, model_s
+
+def img_processing (img_path): 
+    """
+    Preprocess the WSI SR-microCT image following the methodology from:
+
+        Poles, Isabella, et al. 
+        "On How to Unravel Bone Microscale Phenomena: 
+        A Mask-Guided Attention SR-microCT Image Classification Approach." 
+        2023 IEEE EMBS International Conference on Biomedical and Health Informatics (BHI). IEEE, 2023.
+
+    Arguments:
+        img_path (str): Path to the SR-microCT image to preprocess
+
+    Returns:
+        preprocessed_img (ndarray or tensor): Preprocessed image ready for downstream tasks
+    """
+    img = np.asarray(Image.open(img_path))
+    img_norm = cv2.normalize(img, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
+    img_adj = imadjust(img_norm)
+    img_gaus = gaussian_filter(img_adj, sigma = 2)
+
+    # [0, 1] intensity range 
+    if (np.sum(np.array(img) >= 0) > np.sum(np.array(img) <= 0)): 
+        val = filters.threshold_otsu(img_gaus)
+
+        img_bin = img_gaus > val
+        img_bin = np.array(img_bin).astype(bool)
+    else: 
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+        k = 3
+        im_v = np.reshape(img_gaus, (img_gaus.shape [0]*img_gaus.shape [1],1))
+
+        _, labels, (centers) = cv2.kmeans(im_v, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        centers = np.uint8(centers)
+        labels = (labels).flatten()
+
+        segmented_image = centers[labels.flatten()]
+
+        seg_img0 = segmented_image==centers[0]
+        seg_img1 = segmented_image==centers[1]
+        seg_img2 = segmented_image==centers[2]
+        seg_res = [seg_img0.reshape(img_gaus.shape), seg_img1.reshape(img_gaus.shape), seg_img2.reshape(img_gaus.shape)]
+        min_pix = np.argmin ((np.sum (seg_res[0]), np.sum (seg_res[1]), np.sum (seg_res[2])))
+
+        img_bin = seg_res[min_pix]
+
+    img_open = ndimage.binary_opening(img_bin, structure = square(20)).astype(bool)
+    mask = ndimage.binary_closing(img_open, structure = disk(25)).astype(bool)
+
+    img_adj[mask==0] = 0
+    img_fin = img_adj.astype('uint8')
+
+    return img_fin
+
+def imadjust(src, tol = 1, vin = [0,255], vout = (0,255)):
+    """
+    Adjust the intensity values of a grayscale image to enhance contrast.
+
+    By default, this function saturates the bottom 1% and the top 1% of all pixel values. 
+    It then linearly maps the pixel values between these limits to a new range [0, 1], 
+    effectively increasing the contrast of the output image.
+
+    Arguments:
+        src (ndarray): Input grayscale image
+        tol (float): Tolerance for saturation (default is 1%)
+        vin (tuple): Input intensity range (e.g., (min, max)); if None, calculated from image
+        vout (tuple): Output intensity range (default is (0, 1))
+
+    Returns:
+        dst (ndarray): Contrast-enhanced image
+    """
+    assert len(src.shape) == 2 ,'Input image should be 2-dims'
+
+    tol = max(0, min(100, tol))
+
+    if tol > 0:
+        # Compute in and out limits
+        # Histogram
+        hist = np.histogram(src,bins=list(range(256)),range=(0,255))[0]
+
+        # Cumulative histogram
+        cum = hist.copy()
+        for i in range(1, 255): 
+            cum[i] = cum[i - 1] + hist[i]
+
+        # Compute bounds
+        total = src.shape[0] * src.shape[1]
+        low_bound = total * tol / 100
+        upp_bound = total * (100 - tol) / 100
+        vin[0] = bisect.bisect_left(cum, low_bound)
+        vin[1] = bisect.bisect_left(cum, upp_bound)
+
+    # Stretching
+    scale = (vout[1] - vout[0]) / (vin[1] - vin[0])
+    vs = src-vin[0]
+    vs[src<vin[0]]=0
+    vd = vs*scale+0.5 + vout[0]
+    vd[vd>vout[1]] = vout[1]
+    dst = vd
+
+    return dst
